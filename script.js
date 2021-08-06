4e671533591f3a5ebe38f85486189e4a53c50779
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-08-06 17:31:59 +0200

    login aggiunto

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var v = ["giorgio_casaregola@pippomail.com", "mariafilippa21122012@pippomail.com"."persona_casuale_89@topolino.com", "bagginsfrodo_lotr@mordor.com"];
+const v = ["giorgio_casaregola@pippomail.com", "mariafilippa21122012@pippomail.com", "persona_casuale_89@topolino.com", "bagginsfrodo_lotr@mordor.com", "jhondoe404@pippomail.com"];
+
+let mail = prompt("Email per l'accesso");
+let verifica = false;
+for (var i=0; i<5 ;i++){
+    if(mail==v[i]){
+        verifica=true;
+    }
+}
+
+
+if(verifica){
+    document.getElementById("accesso_eseguito").innerHTML =  "Accesso eseguito con successo";
+}
+else{
+    document.getElementById("accesso_negato").innerHTML =  "Accesso fallito... email non riconosciuta";
+}
+
+
